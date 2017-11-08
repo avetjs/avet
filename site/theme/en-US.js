fe9cf7a6ac4d4bbb495426714747fc9215424510
okoala <dapixp@gmail.com>
2017-11-08 19:58:10 +0800

    docs: finish footer

--- site/theme/en-US.js
+++ site/theme/en-US.js
@@ -1,4 +1,4 @@
-'use strict';
+﻿'use strict';
 
 const appLocaleData = require('react-intl/locale-data/en');
 
@@ -35,19 +35,12 @@ module.exports = {
     'app.home.sub-slogan': 'A Little Happiness in Hand',
     'app.home.vision': 'This is a design language dedicated to improve the user and design experience.',
     'app.footer.repo': 'GitHub Repository',
-    'app.footer.awesome': 'Awesome Ant Design',
+    'app.footer.awesome': 'Awesome Avet',
     'app.footer.primary-color-changed': 'Change primary color successfully!',
-    'app.footer.scaffold': 'Scaffold',
-    'app.footer.scaffolds': 'Scaffold Market',
     'app.footer.dev-tools': 'Developer Tools',
-    'app.footer.dva': 'Framework',
     'app.footer.resources': 'Resources',
-    'app.footer.data-vis': 'Data Visualization',
     'app.footer.eggjs': 'Enterprise Node Framework',
-    'app.footer.motion': 'Motion Solution',
-    'app.footer.antd-library': 'Axure library',
-    'app.footer.design-platform': 'Ant Financial Design Platform',
-    'app.footer.antux': 'Sitemap Template',
+    'app.footer.nextjs': 'Server Rendered React applications',
     'app.footer.community': 'Community',
     'app.footer.change-log': 'Change Log',
     'app.footer.faq': 'FAQ',
@@ -59,8 +52,8 @@ module.exports = {
     'app.footer.bug-report': 'Bug Report',
     'app.footer.issues': 'Issues',
     'app.footer.version': 'Version: ',
-    'app.footer.author': 'Created by AFX',
-    'app.publish.title': 'antd@2.0.0 is released!',
+    'app.footer.author': 'Created by 仙森 @okoala',
+    'app.publish.title': 'avet is released!',
     'app.publish.greeting': 'Hello, ',
     'app.publish.intro': ' is released, and please upgrade. ',
     'app.publish.old-version-guide': 'If you want to read old version documentation, please visit ',
--- site/theme/template/Layout/Footer.jsx
+++ site/theme/template/Layout/Footer.jsx
@@ -14,136 +14,29 @@ class Footer extends React.Component {
     };
   }
 
-  componentDidMount() {
-    // for some iOS
-    // http://stackoverflow.com/a/14555361
-    if (!isLocalStorageNameSupported()) {
-      return;
-    }
-    // 大版本发布后全局弹窗提示
-    //   1. 点击『知道了』之后不再提示
-    //   2. 超过截止日期后不再提示
-    if (
-      localStorage.getItem('antd@2.0.0-notification-sent') !== 'true' &&
-        Date.now() < new Date('2016/10/14').getTime()
-    ) {
-      this.infoNewVersion();
-    }
-  }
-
-  handleColorChange = (color) => {
-    const changeColor = () => {
-      const { messages } = this.props.intl;
-      window.less.modifyVars({
-        '@primary-color': color,
-      }).then(() => {
-        message.success(messages['app.footer.primary-color-changed']);
-        this.setState({ color });
-      });
-    };
-
-    const lessUrl = 'https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.2/less.min.js';
-
-    if (this.lessLoaded) {
-      changeColor();
-    } else {
-      window.less = {
-        async: true,
-      };
-      loadScript(lessUrl).then(() => {
-        this.lessLoaded = true;
-        changeColor();
-      });
-    }
-  }
-
-  infoNewVersion() {
-    const { messages } = this.props.intl;
-    Modal.info({
-      title: messages['app.publish.title'],
-      content: (
-        <div>
-          <img src="https://os.alipayobjects.com/rmsportal/nyqBompsynAQCpJ.svg" alt="Ant Design" />
-          <p>
-            {messages['app.publish.greeting']}
-            <a target="_blank" rel="noopener noreferrer" href="/changelog">antd@2.0.0</a>
-            {messages['app.publish.intro']}
-            {messages['app.publish.old-version-guide']}
-            <a target="_blank" rel="noopener noreferrer" href="http://1x.ant.design">1x.ant.design</a>
-            {messages['app.publish.old-version-tips']}
-          </p>
-        </div>
-      ),
-      okText: 'OK',
-      onOk: () => localStorage.setItem('antd@2.0.0-notification-sent', 'true'),
-      className: 'new-version-info-modal',
-      width: 470,
-    });
-  }
-
   render() {
     return (
       <footer id="footer">
         <ul>
           <li>
-            <h2><Icon type="github" /> Ant Design</h2>
+            <h2><Icon type="github" /> Avet</h2>
             <div>
-              <a target="_blank " href="https://github.com/ant-design/ant-design">
+              <a target="_blank " href="https://github.com/avetjs/avet">
                 GitHub
-              </a>
-            </div>
-            <div>
-              <a href="http://mobile.ant.design">Ant Design Mobile</a>
-            </div>
-            <div>
-              <a href="http://ng.ant.design">NG-ZORRO</a>
-              <span> - </span>
-              Ant Design of Angular
-            </div>
-            <div>
-              <a target="_blank " href="https://github.com/websemantics/awesome-ant-design">
-                <FormattedMessage id="app.footer.awesome" />
               </a>
             </div>
           </li>
           <li>
             <h2><Icon type="link" /> <FormattedMessage id="app.footer.resources" /></h2>
             <div>
-              <a href="http://scaffold.ant.design">Scaffolds</a>
-              <span> - </span>
-              <FormattedMessage id="app.footer.scaffolds" />
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/dvajs/dva">dva</a> - <FormattedMessage id="app.footer.dva" />
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/dvajs/dva-cli">dva-cli</a> -
-              <FormattedMessage id="app.footer.dev-tools" />
-            </div>
-            <div>
               <a target="_blank" rel="noopener noreferrer" href="https://eggjs.org/">Egg</a>
               <span> - </span>
               <FormattedMessage id="app.footer.eggjs" />
             </div>
             <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://antv.alipay.com/">AntV</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/zeit/next.js/">Next.js</a>
               <span> - </span>
-              <FormattedMessage id="app.footer.data-vis" />
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="http://motion.ant.design">Ant Motion</a>
-              <span> - </span>
-              <FormattedMessage id="app.footer.motion" />
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="http://library.ant.design/">AntD Library</a>
-              <span> - </span>
-              <FormattedMessage id="app.footer.antd-library" />
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="http://ux.ant.design">Ant UX</a>
-              <span> - </span>
-              <FormattedMessage id="app.footer.antux" />
+              <FormattedMessage id="app.footer.nextjs" />
             </div>
           </li>
           <li>
@@ -154,38 +47,13 @@ class Footer extends React.Component {
               </a>
             </div>
             <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/ant-design/ant-design/wiki/FAQ">
-                <FormattedMessage id="app.footer.faq" />
-              </a>
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://gitter.im/ant-design/ant-design">
-                <FormattedMessage id="app.footer.discuss-cn" />
-              </a>
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://gitter.im/ant-design/ant-design-english">
-                <FormattedMessage id="app.footer.discuss-en" />
-              </a>
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="http://new-issue.ant.design/">
-                <FormattedMessage id="app.footer.bug-report" />
-              </a>
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/ant-design/ant-design/issues">
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/avetjs/avet/issues">
                 <FormattedMessage id="app.footer.issues" />
               </a>
             </div>
             <div>
-              <a target="_blank" rel="noopener noreferrer" href="http://stackoverflow.com/questions/tagged/antd">
+              <a target="_blank" rel="noopener noreferrer" href="http://stackoverflow.com/questions/tagged/avet">
                 <FormattedMessage id="app.footer.stackoverflow" />
-              </a>
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://segmentfault.com/t/antd">
-                <FormattedMessage id="app.footer.segmentfault" />
               </a>
             </div>
           </li>
