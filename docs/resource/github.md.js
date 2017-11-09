82f8465cd585a8b7afd92e8ad901eca7467ba44d
okoala <dapixp@gmail.com>
2017-11-09 10:38:13 +0800

    Updates

--- docs/resource/github.md.js
+++ docs/resource/github.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([2],{1497:function(t,e){t.exports={content:["article"],meta:{order:2,link:"//github.com/avetjs/avet",title:"GitHub",filename:"docs/resource/github.md"},toc:["ul"]}}});
+webpackJsonp([2],{1331:function(t,e){t.exports={content:["article"],meta:{order:2,link:"//github.com/avetjs/avet",title:"GitHub",filename:"docs/resource/github.md"},toc:["ul"]}}});
