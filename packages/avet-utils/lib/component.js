850c8694d3ac6b8a900eb2d98cdf67eb8c276f92
仙森 <dapixp@gmail.com>
2017-12-06 19:58:45 +0800

    Remove async for bundle and use lib folder file (#18)

--- packages/avet-utils/lib/component.js
+++ packages/avet-utils/lib/component.js
@@ -2,17 +2,24 @@ function getDisplayName(Component) {
   return Component.displayName || Component.name || 'UnKnown';
 }
 
-async function loadGetInitialProps(Component, ctx) {
-  if (!Component.getInitialProps) return {};
+function loadGetInitialProps(Component, ctx) {
+  return new Promise(function(resolve, reject) {
+    if (!Component.getInitialProps) return resolve({});
 
-  const props = await Component.getInitialProps(ctx);
-  if (!props && (!ctx.res || !ctx.res.finished)) {
-    const compName = getDisplayName(Component);
-    const message = `"${compName}.getInitialProps()" should resolve to an object. But found "${props}" instead.`;
-    throw new Error(message);
-  }
+    Component.getInitialProps(ctx).then(function(props) {
+      if (!props && (!ctx.res || !ctx.res.finished)) {
+        const compName = getDisplayName(Component);
+        const message = `"${
+          compName
+        }.getInitialProps()" should resolve to an object. But found "${
+          props
+        }" instead.`;
+        return reject(message);
+      }
 
-  return props;
+      return resolve(props);
+    })
+  })
 }
 
 module.exports = {
