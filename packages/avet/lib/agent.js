6f53337a8233a62e3fc93b8e73336c13ac094f45
okoala <dapixp@gmail.com>
2017-12-27 00:00:31 +0800

    f

--- packages/avet/lib/agent.js
+++ packages/avet/lib/agent.js
@@ -6,76 +6,16 @@ const AVET_PATH = Symbol.for('avet#avetPath');
 
 class Agent extends AvetCore {
   constructor(options = {}) {
-    options.type = 'agent';
-
     super(options);
-
-    this.messenger = new Messenger();
-
-    this._wrapMessenger();
 
     this.loader.loadPlugin();
     this.loader.loadConfig();
     this.loader.loadCustomAgent();
     this.loader.loadExtend();
-
-    // keep agent alive even it don't have any io tasks
-    setInterval(() => {}, 24 * 60 * 60 * 1000);
-
-    this._uncaughtExceptionHandler = this._uncaughtExceptionHandler.bind(this);
-    process.on('uncaughtException', this._uncaughtExceptionHandler);
-  }
-
-  _uncaughtExceptionHandler(err) {
-    if (!(err instanceof Error)) {
-      err = new Error(String(err));
-    }
-    /* istanbul ignore else */
-    if (err.name === 'Error') {
-      err.name = 'unhandledExceptionError';
-    }
-    console.error(err);
   }
 
   get [AVET_PATH]() {
     return path.join(__dirname, '..');
-  }
-
-  _wrapMessenger() {
-    for (const methodName of [
-      'broadcast',
-      'sendTo',
-      'sendToApp',
-      'sendToAgent',
-      'sendRandom',
-    ]) {
-      wrapMethod(methodName, this.messenger);
-    }
-
-    function wrapMethod(methodName, messenger) {
-      const originMethod = messenger[methodName];
-      messenger[methodName] = function(...args) {
-        const stack = new Error().stack
-          .split('\n')
-          .slice(1)
-          .join('\n');
-
-        console.warn(
-          "agent can't call %s before server started\n%s",
-          methodName,
-          stack
-        );
-        originMethod.apply(this, args);
-      };
-      messenger.once('egg-ready', () => {
-        messenger[methodName] = originMethod;
-      });
-    }
-  }
-
-  close() {
-    process.removeListener('uncaughtException', this._uncaughtExceptionHandler);
-    return super.close();
   }
 }
 
--- packages/avet/lib/application.js
+++ packages/avet/lib/application.js
@@ -1,18 +1,21 @@
 const path = require('path');
-const { AvetCore } = require('avet-core');
+const { AppWorkerLoader } = require('./loader');
 
 const AVET_PATH = Symbol.for('avet#avetPath');
+const EGG_LOADER = Symbol.for('egg#loader');
 
-class Application extends AvetCore {
+class Application extends AppWorkerLoader {
   constructor(options) {
-    options.type = 'application';
-
     super(options);
 
     this.loader.loadPlugin();
     this.loader.loadConfig();
     this.loader.loadCustomApp();
     this.loader.loadAvetExtend();
+  }
+
+  get [EGG_LOADER]() {
+    return AppWorkerLoader;
   }
 
   get [AVET_PATH]() {
