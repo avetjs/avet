d0de49fd4a2321cb0d867e9a7f354a74c9b3c9b1
okoala <dapixp@gmail.com>
2017-12-27 13:38:29 +0800

    f

--- packages/avet/index.js
+++ packages/avet/index.js
@@ -1,17 +1,16 @@
 const egg = require('egg');
 
-const framework = {};
+module.exports = exports = egg;
+
 /**
  * build avet application
  */
-framework.buildApp = require('./lib/build');
+exports.buildApp = require('./lib/build');
 /**
  * Start avet application with cluster mode
  */
-framework.startCluster = require('avet-cluster').startCluster;
-framework.Application = require('./lib/application');
-framework.Agent = require('./lib/agent');
-framework.AppWorkerLoader = require('./lib/core/loader/app_worker_loader');
-framework.AgentWorkerLoader = require('./lib/core/loader/agent_worker_loader');
-
-module.exports = exports = Object.assign(egg, framework);
+exports.startCluster = require('avet-cluster').startCluster;
+exports.Application = require('./lib/application');
+exports.Agent = require('./lib/agent');
+exports.AppWorkerLoader = require('./lib/core/loader/app_worker_loader');
+exports.AgentWorkerLoader = require('./lib/core/loader/agent_worker_loader');
