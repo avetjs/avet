2603be47e8a9557c42bfc96db11dfb50e9360940
okoala <dapixp@gmail.com>
2017-12-27 15:09:58 +0800

    f

--- packages/avet/app/extend/context.js
+++ packages/avet/app/extend/context.js
@@ -1,13 +1,9 @@
-const AvetServer = require('avet-server/lib/server');
-const { getAverConfiguration } = require('../../lib/utils');
-
 const AVET_SERVER = Symbol('Context#avetServer');
 
 module.exports = {
   get avetServer() {
     if (!this[AVET_SERVER]) {
-      const config = getAverConfiguration(this);
-      this[AVET_SERVER] = new AvetServer(config);
+      this[AVET_SERVER] = this.app.avetServer;
     }
 
     return this[AVET_SERVER];
