b481ccbaa77a5b634754d0ae3e932a0023073312
okoala <dapixp@gmail.com>
2018-01-10 10:57:50 +0800

    f

--- packages/avet-bin/lib/jest/puppeteer_environment.js
+++ packages/avet-bin/lib/jest/puppeteer_environment.js
@@ -1,4 +1,3 @@
-const chalk = require('chalk');
 const NodeEnvironment = require('jest-environment-node');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
@@ -9,7 +8,6 @@ const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
 class PuppeteerEnvironment extends NodeEnvironment {
   async setup() {
-    console.log(chalk.yellow('Setup Test Environment.'));
     await super.setup();
     const wsEndpoint = fs.readFileSync(path.join(DIR, 'wsEndpoint'), 'utf8');
     if (!wsEndpoint) {
@@ -21,7 +19,6 @@ class PuppeteerEnvironment extends NodeEnvironment {
   }
 
   async teardown() {
-    console.log(chalk.yellow('Teardown Test Environment.'));
     await super.teardown();
   }
 
