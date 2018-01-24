595645ab966450a3245e3b94e68543daa0da55e7
okoala <dapixp@gmail.com>
2018-01-24 20:39:09 +0800

    feat: remove comment

--- packages/avet/lib/application.js
+++ packages/avet/lib/application.js
@@ -5,15 +5,10 @@ const { getAverConfiguration } = require('./utils');
 
 const EGG_LOADER = Symbol.for('egg#loader');
 const EGG_PATH = Symbol.for('egg#eggPath');
-// const EGG_READY_TIMEOUT_ENV = Symbol('EggCore#eggReadyTimeoutEnv');
 
 class Application extends EggApplication {
   constructor(...opts) {
     super(...opts);
-    // this[EGG_READY_TIMEOUT_ENV] = Number.parseInt(
-    //   process.env.EGG_READY_TIMEOUT_ENV || 20000,
-    //   10
-    // );
 
     // don't run in build env
     if (process.env.AVET_RUN_ENV !== 'build') {
