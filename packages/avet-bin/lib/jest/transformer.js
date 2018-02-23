04fb6897dc59d83ccde4d1f5be185d9e9e977cff
okoala <dapixp@gmail.com>
2018-02-23 19:50:42 +0800

    feat: add some babel plugins transform

--- packages/avet-bin/lib/jest/transformer.js
+++ packages/avet-bin/lib/jest/transformer.js
@@ -5,4 +5,9 @@ module.exports = babelJest.createTransformer({
     require.resolve('babel-preset-env'),
     require.resolve('babel-preset-react'),
   ],
+  plugins: [
+    require.resolve('babel-plugin-transform-class-properties'),
+    require.resolve('babel-plugin-transform-object-rest-spread'),
+    require.resolve('babel-plugin-transform-runtime'),
+  ],
 });
